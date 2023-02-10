bc772250a9ec094033d8490eff4286fc497aaf6f
Olugbenga Aderoba <100159760+Olugbenga-Aderoba@users.noreply.github.com>
2023-02-10 18:48:57 +0100

    Update urls.py

--- e_voting/urls.py
+++ e_voting/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('administrator/', include('administrator.urls')),
     path('voting/', include('voting.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
