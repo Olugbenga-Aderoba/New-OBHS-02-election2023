a574c13103c4be2604ebd7b4478a75fbae10b896
Olugbenga Aderoba <100159760+Olugbenga-Aderoba@users.noreply.github.com>
2023-02-14 16:47:48 +0100

    Update settings.py

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'account.middleware.AccountCheckMiddleWare',
 ]
 
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
